ea8eaeefeff69303358521cafa6073b9b66c86df
Srihith <110789786+SrihithYarabati@users.noreply.github.com>
2022-12-22 21:04:02 +0000

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,9 @@
+// random value generated
+  var y = Math.floor(Math.random() * 10+1)
+// counting the number of guesses
+// made for correct Guess
+
+  
+// function for number guessed by user     
+var x = document.getElementById("guessFeild").value;
+
